469549934c14c9904161f88fecc21d9d011e1528
Sultan Sidhu <40362692+sultansidhu@users.noreply.github.com>
2018-09-22 14:59:33 -0400

    Add files via upload

--- smartpark_gps_module.py
+++ smartpark_gps_module.py
@@ -14,10 +14,61 @@ instance = Geocoding(api_key="<your api key>")  # todo: read api key from a txt 
 file = open("/Users/sultansidhu/Desktop/DataFiles/greenPParking2015.json")
 stuff = file.read()
 jsonstuff = json.loads(stuff)
-for x in jsonstuff['carparks']:
-    print(x, end="\n\n\n\n\n")
-#print(jsonstuff['carparks'][0])
 # for x in jsonstuff['carparks']:
-#     print(x['address'])
-# print(len(jsonstuff['carparks']))
+#     print(x, end="\n\n\n\n\n")
 
+
+class ParkingClass:
+
+    def __init__(self, location: tuple, address: str, rate_per_half_hour: float, capacity: int):
+        """
+        Creates a new Parking object
+        """
+        self.location = location
+        self.address = address
+        self.rate_per_hour = rate_per_half_hour
+        self.capacity = capacity
+        self.proximity_rating = 0
+        self.rph_rating = 0
+        self.payment_options = []
+
+    def __str__(self) -> str:
+        """
+        Returns a String representation of this Parking object
+        """
+        return "Latitude: {}, Longitude: {}, Address: {}, Rate: {}, Capacity: {}, Payment Options: {}".format(self.location[0],
+                                                                           self.location[1],
+                                                                           self.address,
+                                                                           self.rate_per_hour,
+                                                                           self.capacity,
+                                                                           self.payment_options)
+
+    def getLocationTuple(self) -> tuple:
+        """
+        Returns a tuple containing two floats representing the latitude and longitude of this parking
+        """
+        return self.location
+
+    def setPaymentOptions(self, payment_option_list):
+        """Sets the payment options for a specific object."""
+        self.payment_options = payment_option_list
+
+
+def createParkingArray(jason):
+    """Creates an array of all parking objects."""
+    parkingArray = []
+    for i in range(len(jason['carparks'])):
+        address = jason['carparks'][i]['address']
+        lattitude = jason['carparks'][i]['lat']
+        longitude = jason['carparks'][i]['lng']
+        rate = jason['carparks'][i]['rate_half_hour']
+        capacity = jason['carparks'][i]['capacity']
+        new_object = ParkingClass((lattitude, longitude), address, rate, capacity)
+        new_object.setPaymentOptions(jason['carparks'][i]['payment_options'])
+        parkingArray.append(new_object)
+    return parkingArray
+
+thestuff = createParkingArray(jsonstuff)
+print(len(thestuff))
+for x in thestuff:
+    print(x, end="\n\n")
